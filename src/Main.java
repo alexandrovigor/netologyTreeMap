81eead8608a3baf01a07eeec2b22a54f28b44897
alexandrovigor <gsandy67@mail.ru>
2022-11-04 06:59:09 +0300

    Add comporator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Михаил", "Бестужев-Рюмин", 25));
+        people.add(new Person("Сергей", "Муравьев-Апостол", 30));
+        people.add(new Person("Павел", "Пестель", 33));
+        people.add(new Person("Петр", "Каховский", 29));
+        people.add(new Person("Кондратий", "Рылеев", 31));
+        people.add(new Person("Николай", "Император Самодержец Всероссийский", 30));
+        
+        Collections.sort(people, new StatusComporator());
+        int i;
+        for (i = 0; i < people.size(); i++) {
+            System.out.println((i + 1) + "." + people.get(i));
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,39 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String toString() {
+        return name + " " + surname + " " + "( " + age + " )";
+    }
+
+    public static int surnameSize(String getSurname) {
+        Set<String> words;
+        String s1 = getSurname.replace("-", " ");
+        words = new HashSet<>(List.of(s1.split("\\P{IsAlphabetic}+")));
+        return words.size();
+    }
+
+
 }
+
+

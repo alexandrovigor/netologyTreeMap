88371fa231b81a23db800306737dd482c8f2ff05
alexandrovigor <gsandy67@mail.ru>
2022-11-10 05:47:31 +0300

    Add lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -12,7 +13,15 @@ public class Main {
         people.add(new Person("Кондратий", "Рылеев", 31));
         people.add(new Person("Николай", "Император Самодержец Всероссийский", 30));
 
-        Collections.sort(people, new StatusComporator());
+
+        Comparator<Person> comparator= ( o1,  o2)-> {
+                if (Integer.compare(Person.surnameSize(o1.getSurname()), Person.surnameSize(o2.getSurname())) != 0) {
+                    return Integer.compare(Person.surnameSize(o1.getSurname()), Person.surnameSize(o2.getSurname()));
+                }
+                return Integer.compare(o1.getAge(), o2.getAge());
+
+        };
+         Collections.sort(people, comparator);
         int i;
         for (i = 0; i < people.size(); i++) {
             System.out.println((i + 1) + "." + people.get(i));
